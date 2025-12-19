59775a6580fca3d81184c5bf1bea07221f3107a4
Moses Borore <mosesbororekinuthia@gmail.com>
2025-12-19 13:28:01 +0300

    Add sidebar

---
+++ src/lib/components/app-sidebar.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import HouseIcon from "@lucide/svelte/icons/house";
+  import SettingsIcon from "@lucide/svelte/icons/settings";
+  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import * as Avatar from "$lib/components/ui/avatar/index.js";
+  //Menu items
+
+  const items = [
+    { title: "Dashboard", url: "#", icon: HouseIcon },
+    {
+      title: "Settings",
+      url: "#",
+      icon: SettingsIcon,
+    },
+  ];
+</script>
+
+<Sidebar.Root>
+  <Sidebar.Header>
+    <Sidebar.Menu>
+      <Sidebar.MenuItem>
+        <Sidebar.MenuButton>
+          <span class="font-bold text-lg">NeoSpend</span>
+        </Sidebar.MenuButton>
+      </Sidebar.MenuItem>
+    </Sidebar.Menu>
+  </Sidebar.Header>
+  <Sidebar.Content>
+    <Sidebar.Group />
+    <Sidebar.GroupLabel>Overview</Sidebar.GroupLabel>
+    <Sidebar.GroupContent>
+      <Sidebar.Menu>
+        {#each items as item (item.title)}
+          <Sidebar.MenuItem>
+            <Sidebar.MenuButton>
+              {#snippet child({ props })}
+                <a href={item.url} {...props}>
+                  <item.icon />
+                  <span>{item.title}</span>
+                </a>
+              {/snippet}
+            </Sidebar.MenuButton>
+          </Sidebar.MenuItem>
+        {/each}
+      </Sidebar.Menu>
+    </Sidebar.GroupContent>
+
+    <Sidebar.Group />
+  </Sidebar.Content>
+  <Sidebar.Footer>
+    <Avatar.Root>
+      <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+      <Avatar.Fallback>CN</Avatar.Fallback>
+    </Avatar.Root>
+  </Sidebar.Footer>
+</Sidebar.Root>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,17 +4,19 @@
 
   import favicon from "$lib/assets/favicon.svg";
 
+  import type { LayoutProps } from "./$types";
   import { page } from "$app/state";
 
-  import Navbar from "$lib/components/Navbar.svelte";
-  import { ModeWatcher } from "mode-watcher";
+  import { getFlash } from "sveltekit-flash-message";
   import * as Alert from "$lib/components/ui/alert/index.js";
-  import CheckCircle2Icon from "@lucide/svelte/icons/check-circle-2";
+  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+
   import X from "@lucide/svelte/icons/x";
   import CircleAlertIcon from "@lucide/svelte/icons/circle-alert";
+  import CheckCircle2Icon from "@lucide/svelte/icons/check-circle-2";
 
-  import { getFlash } from "sveltekit-flash-message";
-  import type { LayoutProps } from "./$types";
+  import Navbar from "$lib/components/Navbar.svelte";
+  import AppSidebar from "$lib/components/app-sidebar.svelte";
 
   const flash = getFlash(page);
 
@@ -32,35 +34,51 @@
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-<ModeWatcher />
+<Sidebar.Provider>
+  <AppSidebar />
+  <div class="bg-background flex flex-col relative min-h-svh w-full">
+    <header
+      class="bg-background/90 sticky top-0 z-30 w-full backdrop-blur transition-shadow duration-100"
+    >
+      <div class="flex items-center p-1">
+        <Sidebar.Trigger />
+        <div class="flex-1">
+          <Navbar isAuthenticated={data.isAuthenticated} />
+        </div>
+      </div>
+    </header>
 
-<Navbar isAuthenticated={data.isAuthenticated} />
+    {#if $flash}
+      <section>
+        <div class="container mx-auto mt-2" bind:this={alertContainer}>
+          <div class="grid w-full max-w-xl items-start gap-4 mx-auto">
+            <Alert.Root
+              variant={$flash.type === "error" ? "destructive" : "default"}
+              class="flex"
+            >
+              {#if $flash.type === "success"}
+                <CheckCircle2Icon />
+              {:else}
+                <CircleAlertIcon class="size-4" />
+              {/if}
+              <Alert.Title>{$flash.message.title}</Alert.Title>
+              {#if $flash.message.description}
+                <Alert.Description
+                  >{$flash.message.description}</Alert.Description
+                >
+              {/if}
 
-{#if $flash}
-  <div class="container mx-auto mt-4" bind:this={alertContainer}>
-    <div class="grid w-full max-w-xl items-start gap-4 mx-auto">
-      <Alert.Root
-        variant={$flash.type === "error" ? "destructive" : "default"}
-        class="flex"
-      >
-        {#if $flash.type === "success"}
-          <CheckCircle2Icon />
-        {:else}
-          <CircleAlertIcon class="size-4" />
-        {/if}
-        <Alert.Title>{$flash.message.title}</Alert.Title>
-        {#if $flash.message.description}
-          <Alert.Description>{$flash.message.description}</Alert.Description>
-        {/if}
+              <span class="ms-auto">
+                <button onclick={() => dismissAlert()}> <X size="18" /></button>
+              </span>
+            </Alert.Root>
+          </div>
+        </div>
+      </section>
+    {/if}
 
-        <span class="ms-auto">
-          <button onclick={() => dismissAlert()}> <X size="18" /></button>
-        </span>
-      </Alert.Root>
-    </div>
+    <main class="container mx-auto p-4 flex-1" id="mainContent">
+      {@render children()}
+    </main>
   </div>
-{/if}
-
-<div class="container mx-auto p-4">
-  {@render children()}
-</div>
+</Sidebar.Provider>
