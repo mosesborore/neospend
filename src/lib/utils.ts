f383617183bfa755fa7cb524fda2dccb57c2e4d0
Moses Borore <mosesbororekinuthia@gmail.com>
2025-12-30 13:34:13 +0300

    Add number formatter and func to create redirect to url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,27 @@
+import type { RequestEvent } from "@sveltejs/kit";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
+}
+
+export function formatAmount(amount: number) {
+  return Intl.NumberFormat().format(amount);
+}
+
+export function handleLoginRedirect(event: RequestEvent) {
+  const nextTo = event.url.pathname + event.url.search;
+  return `/auth/login?next=${nextTo}`;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type WithoutChild<T> = T extends { child?: any } ? Omit<T, "child"> : T;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type WithoutChildren<T> = T extends { children?: any } ? Omit<T, "children"> : T;
+export type WithoutChildren<T> = T extends { children?: any }
+  ? Omit<T, "children">
+  : T;
 export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
-export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & { ref?: U | null };
+export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & {
+  ref?: U | null;
+};
