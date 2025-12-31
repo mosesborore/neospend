b184879a9ea0f4b8e4f687aa0ba9ea44a7c69a0d
Moses Borore <mosesbororekinuthia@gmail.com>
2025-12-31 12:02:28 +0300

    Add transfer list component

---
+++ src/lib/components/transfer-list.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { Transfer } from "$lib/types";
+  import TransferItem from "./transfer-item.svelte";
+
+  let { transfers }: { transfers: Transfer[] } = $props();
+</script>
+
+<div class="flex flex-col gap-4 overflow-y-auto max-h-[80svh]">
+  {#if transfers.length > 0}
+    {#each transfers as transfer}
+      <TransferItem {transfer} />
+    {/each}
+  {:else}
+    <p class="text-center text-muted-foreground text-sm">
+      No transfers to display.
+    </p>
+  {/if}
+</div>
