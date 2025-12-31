75050b14f31f281b414b0f03fef2e5f1cca01712
Moses Borore <mosesbororekinuthia@gmail.com>
2025-12-31 16:09:12 +0300

    Update schema used for transaction form validation

--- src/routes/transactions/+page.server.ts
+++ src/routes/transactions/+page.server.ts
@@ -1,16 +1,14 @@
 import { message, superValidate } from "sveltekit-superforms";
 import type { Actions, PageServerLoad } from "./$types";
 
-import {
-  accountInsertZodSchema,
-  transactionInsertZodSchema,
-} from "$lib/schemas/index";
+import { CreateTransactionSchema, type SelectInputOption } from "$lib/types";
 import { account as accountTable } from "$lib/server/database/schema";
 import { db } from "$lib/server/database/db";
 import { requireLogin } from "$lib/server/auth";
 import { eq } from "drizzle-orm";
 
-import { setFlash } from "sveltekit-flash-message/server";
+import { fail } from "@sveltejs/kit";
+import { zod4 } from "sveltekit-superforms/adapters";
 
 export const load: PageServerLoad = async (event) => {
   const user = requireLogin(event);
@@ -20,53 +18,35 @@ export const load: PageServerLoad = async (event) => {
     .from(accountTable)
     .where(eq(accountTable.userId, user.id));
 
-  const addTransactionForm = await superValidate(transactionInsertZodSchema);
+  const getAccountOptions = () => {
+    let c: SelectInputOption[] = [];
 
-  const accountForm = await superValidate(accountInsertZodSchema);
-  return { addTransactionForm, accountForm, accounts };
+    accounts.forEach((a) => {
+      c.push({ value: a.id, label: a.name });
+    });
+
+    return c;
+  };
+
+  const addTransactionForm = await superValidate(
+    event.request,
+    zod4(CreateTransactionSchema)
+  );
+
+  return { addTransactionForm, accounts, accountOptions: getAccountOptions() };
 };
 
 export const actions: Actions = {
   addTransaction: async (event) => {
-    console.log(await event.request.formData());
-
-    const form = await superValidate(event.request, transactionInsertZodSchema);
-
-    if (!form.valid) {
-      return message(form, "Please fill the form properly.");
-    }
-
-    return { message: "Data received" };
-  },
-  addAccount: async (event) => {
-    const user = requireLogin(event);
-
-    const form = await superValidate(event.request, accountInsertZodSchema);
-
-    if (!form.valid) {
-      return message(form, "Please fill the form properly.");
-    }
-    const data = {
-      name: form.data.name,
-      balance: form.data.balance,
-      userId: user.id,
-    };
-
-    try {
-      await db.insert(accountTable).values(data);
-    } catch {
-      return message(form, "Unable to add account.");
-    }
-
-    setFlash(
-      {
-        type: "success",
-        message: {
-          title: `${form.data.name}: New account added successfully.`,
-        },
-      },
-      event
+    const form = await superValidate(
+      event.request,
+      zod4(CreateTransactionSchema)
     );
-    return message(form, "Data received");
+
+    if (!form.valid) {
+      return fail(400, { form });
+    }
+
+    return message(form, "Form posted successfully!");
   },
 };
