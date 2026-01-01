a4eef50088361cac2945d0876cdac09e0a140022
Moses Borore <mosesbororekinuthia@gmail.com>
2026-01-01 17:23:32 +0300

    Add schema to drizzle instance

--- src/lib/server/database/db.ts
+++ src/lib/server/database/db.ts
@@ -3,10 +3,12 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { env } from "$env/dynamic/private";
 import postgres from "pg";
 
+import * as schema from "./schema";
+
 if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
 let pool = new postgres.Pool({
   connectionString: env.DATABASE_URL,
   maxLifetimeSeconds: 300,
 });
-export const db = drizzle(pool);
+export const db = drizzle(pool, { schema: schema });
