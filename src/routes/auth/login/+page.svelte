2899865e1f0a724b1eba963d6d36992f0a064aa8
Moses Borore <mosesbororekinuthia@gmail.com>
2026-01-03 12:57:52 +0300

    Replace generic input fields with Field component

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import type { PageProps } from "./$types";
   import { superForm } from "sveltekit-superforms";
+  import { resolve } from "$app/paths";
 
-  import { Button } from "$lib/components/ui/button/index.js";
-  import { Label } from "$lib/components/ui/label/index.js";
+  import { Button, buttonVariants } from "$lib/components/ui/button/index.js";
   import { Spinner } from "$lib/components/ui/spinner/index.js";
-
+  import * as Field from "$lib/components/ui/field/index.js";
+  import * as Item from "$lib/components/ui/item/index.js";
   import { Input } from "$lib/components/ui/input/index.js";
   import * as Card from "$lib/components/ui/card/index.js";
 
@@ -26,45 +27,59 @@
     <Card.Root class="w-full max-w-sm">
       <Card.Header>
         <Card.Title>Login</Card.Title>
-        <Card.Description>Enter your details login.</Card.Description>
+        <Card.Description>Enter your details to login.</Card.Description>
         <Card.Action>
-          <a href="/auth/register">Register</a>
+          <a
+            class={buttonVariants({ variant: "link" })}
+            href={resolve("/auth/register")}>Register</a
+          >
         </Card.Action>
       </Card.Header>
-      <Card.Content>
+      <Card.Content class="grid gap-y-8">
         {#if $message}
-          <p class="text-xs mb-2 text-center">
-            {$message}
-          </p>
+          <Item.Root variant="outline" size="sm" class="bg-muted">
+            <Item.Content>
+              <Item.Title class="text-muted-foreground">{$message}</Item.Title>
+            </Item.Content>
+          </Item.Root>
         {/if}
 
         <form method="POST" use:enhance>
           <div class="flex flex-col gap-6">
-            <div class="grid gap-2">
-              <Label for="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                name="email"
-                bind:value={$form.email}
-                placeholder="m@example.com"
-                {...$constraints.email}
-              />
-              {#if $errors.email}<span class="invalid">{$errors.email}</span
-                >{/if}
-            </div>
-            <div class="grid gap-2">
-              <div class="flex items-center">
-                <Label for="password">Password</Label>
-              </div>
-              <Input
-                id="password"
-                type="password"
-                name="password"
-                bind:value={$form.password}
-                {...$constraints.password}
-              />
-            </div>
+            <Field.Set>
+              <Field.Group>
+                <Field.Field>
+                  <Field.Label for="email">Email</Field.Label>
+
+                  <Input
+                    id="email"
+                    type="email"
+                    name="email"
+                    placeholder="email@example.com"
+                    bind:value={$form.email}
+                    {...$constraints.email}
+                  />
+                  {#if $errors.email}
+                    <Field.Description>{$errors.email}</Field.Description>
+                  {/if}
+                </Field.Field>
+                <Field.Field>
+                  <Field.Label for="password">Password</Field.Label>
+
+                  <Input
+                    id="password"
+                    type="password"
+                    name="password"
+                    placeholder="••••••••"
+                    bind:value={$form.password}
+                    {...$constraints.password}
+                  />
+                  {#if $errors.password}
+                    <Field.Description>{$errors.password}</Field.Description>
+                  {/if}
+                </Field.Field>
+              </Field.Group>
+            </Field.Set>
             <div class="mt-4">
               <Button variant="default" type="submit" class="w-full">
                 Login
@@ -72,21 +87,10 @@
                   <Spinner />
                 {/if}
               </Button>
-              <Button
-                variant="secondary"
-                class="w-full mt-2 bg-blue-700 text-white"
-                >Login with Google</Button
-              >
             </div>
           </div>
         </form>
       </Card.Content>
-      <!-- <Card.Footer class="flex-col gap-2">
-        <Button variant="default" type="submit" class="w-full"
-          >Create Account</Button
-        >
-        <Button variant="secondary" class="w-full">Create with Google</Button>
-      </Card.Footer> -->
     </Card.Root>
   </div>
 </main>
