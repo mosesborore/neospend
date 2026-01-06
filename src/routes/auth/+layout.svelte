13334cd3fb343781108759a7696dd06a6574efe3
Moses Borore <mosesbororekinuthia@gmail.com>
2026-01-06 18:14:15 +0300

    Add layout page for /auth route

---
+++ src/routes/auth/+layout.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import type { LayoutProps } from "./$types";
+  import * as NavigationMenu from "$lib/components/ui/navigation-menu/index.js";
+  import ThemeToggler from "$lib/components/theme-controller.svelte";
+
+  let { children }: LayoutProps = $props();
+</script>
+
+<div class="container mx-auto">
+  <nav class="flex items-center h-10 py-4">
+    <span class="text-lg font-semibold">NeoSpend</span>
+    <NavigationMenu.Root class="ms-auto">
+      <NavigationMenu.List class="gap-x-2">
+        <NavigationMenu.Item>
+          <NavigationMenu.Link href="/">Home</NavigationMenu.Link>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+          <NavigationMenu.Link href="/auth/login">Login</NavigationMenu.Link>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+          <NavigationMenu.Link href="/auth/register">
+            Register
+          </NavigationMenu.Link>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+          <ThemeToggler />
+        </NavigationMenu.Item>
+      </NavigationMenu.List>
+    </NavigationMenu.Root>
+  </nav>
+</div>
+
+{@render children()}
