c3dd8f6f437265189ce65b9e8e6cb0bec484d962
Moses Borore <mosesbororekinuthia@gmail.com>
2026-01-06 18:14:30 +0300

    Remove nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,6 @@
   import favicon from "$lib/assets/favicon.svg";
 
   import type { LayoutProps } from "./$types";
-  import * as NavigationMenu from "$lib/components/ui/navigation-menu/index.js";
-  import ThemeToggler from "$lib/components/theme-controller.svelte";
-  import { enhance } from "$app/forms";
 
   const isAuthenticated = false;
 
@@ -17,29 +14,5 @@
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
-
-<div class="container mx-auto">
-  <nav class="flex items-center h-10 py-4">
-    <span class="text-lg font-semibold">NeoSpend</span>
-    <NavigationMenu.Root class="ms-auto">
-      <NavigationMenu.List class="gap-x-2">
-        <NavigationMenu.Item>
-          <NavigationMenu.Link href="/">Home</NavigationMenu.Link>
-        </NavigationMenu.Item>
-        <NavigationMenu.Item>
-          <NavigationMenu.Link href="/auth/login">Login</NavigationMenu.Link>
-        </NavigationMenu.Item>
-        <NavigationMenu.Item>
-          <NavigationMenu.Link href="/auth/register">
-            Register
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
-        <NavigationMenu.Item>
-          <ThemeToggler />
-        </NavigationMenu.Item>
-      </NavigationMenu.List>
-    </NavigationMenu.Root>
-  </nav>
-</div>
 
 {@render children()}
