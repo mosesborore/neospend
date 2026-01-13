c90d0ff8e1e565b4f8fae5f1a8483fbc350ddbc0
Moses Borore <mosesbororekinuthia@gmail.com>
2026-01-13 06:42:22 +0300

    Resolve merge conflict in transaction add page

--- src/routes/app/transactions/add/+page.server.ts
+++ src/routes/app/transactions/add/+page.server.ts
@@ -3,14 +3,18 @@ import type { Actions, PageServerLoad } from "./$types";
 
 import { type SelectInputOption } from "$lib/types";
 
-import { CreateTransactionSchema } from "$lib/server/db/types";
-import { accounts as accountTable } from "$lib/server/db/schemas";
+import {
+  accounts as accountTable,
+  transactions,
+  users,
+} from "$lib/server/db/schemas";
 import { db } from "$lib/server/db/db";
 import { requireLogin } from "$lib/server/auth";
-import { eq } from "drizzle-orm";
+import { and, eq, sql } from "drizzle-orm";
 
 import { fail } from "@sveltejs/kit";
 import { zod4 } from "sveltekit-superforms/adapters";
+import { setFlash } from "sveltekit-flash-message/server";
 
 type Options = { id: string | number; name: string; type?: string };
 
@@ -26,6 +30,24 @@ const getSelectOptions = (options: Options[]) => {
 
 export const load: PageServerLoad = async (event) => {
   const user = requireLogin(event);
+
+  const all = await db.query.users.findFirst({
+    columns: {
+      password: false,
+    },
+    where: eq(users.id, user.id),
+    with: {
+      transactions: true,
+
+      accounts: {
+        columns: {
+          userId: false,
+        },
+      },
+    },
+  });
+
+  console.log(all);
 
   const accounts = await db
     .select({
@@ -47,7 +69,12 @@ export const load: PageServerLoad = async (event) => {
 
   const addTransactionForm = await superValidate(
     event.request,
-    zod4(CreateTransactionSchema),
+    zod4(
+      CreateTransactionSchema.extend({
+        userId: CreateTransactionSchema.shape.userId.default(user.id),
+        currency: CreateTransactionSchema.shape.currency.default("kes"),
+      })
+    )
   );
 
   return {
@@ -61,15 +88,64 @@ export const load: PageServerLoad = async (event) => {
 
 export const actions: Actions = {
   addTransaction: async (event) => {
+    const user = requireLogin(event);
+    // console.log(await event.request.formData());
     const form = await superValidate(
       event.request,
       zod4(CreateTransactionSchema),
     );
 
+    console.log(form);
+
     if (!form.valid) {
       return fail(400, { form });
     }
 
-    return message(form, "Form posted successfully!");
+    const expression =
+      form.data.type === "income"
+        ? {
+            balance: sql`${accountTable.balance} + ${form.data.amount}`,
+          }
+        : {
+            balance: sql`${accountTable.balance} - ${form.data.amount}`,
+          };
+
+    try {
+      await db.transaction(async (tx) => {
+        await tx.insert(transactions).values({
+          name: form.data.name,
+          userId: user.id,
+          type: form.data.type,
+          categoryId: Number(form.data.categoryId),
+          accountId: form.data.accountId,
+          amount: form.data.amount,
+          notes: form.data.notes,
+          currency: form.data.currency,
+        });
+
+        await tx
+          .update(accountTable)
+          .set(expression)
+          .where(
+            and(
+              eq(accountTable.userId, user.id),
+              eq(accountTable.id, form.data.accountId)
+            )
+          );
+      });
+
+      setFlash(
+        {
+          type: "success",
+          message: {
+            title: "New transaction added successfully.",
+          },
+        },
+        event
+      );
+      return message(form, "Form posted successfully!");
+    } catch (e) {
+      console.log("Unable to add new transaction: ", e);
+    }
   },
 };
