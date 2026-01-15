57ae40837dad87c712273295b8bf148ff4a8f356
Moses Borore <mosesbororekinuthia@gmail.com>
2026-01-15 21:52:20 +0300

    Add Plus icon

--- src/routes/app/transactions/+page.svelte
+++ src/routes/app/transactions/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { resolve } from "$app/paths";
   import { buttonVariants } from "$lib/components/ui/button/index.js";
+  import { cn } from "$lib/utils";
+  import Plus from "@lucide/svelte/icons/plus";
 </script>
 
 <svelte:head>
@@ -8,7 +10,7 @@
 </svelte:head>
 
 <main>
-  <header class="space-y-4">
+  <header class="flex justify-between items-center">
     <h1
       class="scroll-m-20 text-4xl font-semibold tracking-tight sm:text-3xl xl:text-4xl"
     >
@@ -17,8 +19,10 @@
 
     <a
       href={resolve("/app/transactions/add")}
-      class={buttonVariants({ variant: "default", size: "sm" })}
-      >Add Transaction</a
+      class={cn(buttonVariants({ variant: "default", size: "sm" }), "max-w-xs")}
     >
+      <Plus size={20} />
+      Add Transaction
+    </a>
   </header>
 </main>
